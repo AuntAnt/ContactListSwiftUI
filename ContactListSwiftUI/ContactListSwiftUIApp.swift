fffcaa22553183bedf845ff4054feb3fbd8602f8
Anton Kuzmin <furthertwoant@gmail.com>
2023-05-04 11:50:37 +0400

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Anton Kuzmin on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
